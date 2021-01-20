be6ff45060887beefc89d05d5c674c36173b9bd9
张启蒙 <2293172479@qq.com>
2021-01-20 16:27:25 +0800

    build setting

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: process.env.NODE_ENV === 'production' ? 'https://www.baidu.com' : './',
+        assetsPublicPath: './',
         /**
          * Source Maps
          */
