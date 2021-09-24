8fd8387e248654c8b03c4a7f42131286472eb3cc
张启蒙 <2293172479@qq.com>
2021-09-24 10:32:11 +0800

    comment

--- src/components/common/comment.vue
+++ src/components/common/comment.vue
@@ -8,10 +8,27 @@
         </el-radio-group>
     </div>
     <div class="new-comment">
-        <el-avatar size="large" :src="avatarUrl"></el-avatar>
+        <div class="edit-input">
+            <el-avatar size="large" :src="avatarUrl"></el-avatar>
+            <el-input show-word-limit maxlength="300" type="textarea" :autosize="{ minRows: 2, maxRows: 4}" placeholder="请输入内容" v-model="newComment">
+            </el-input>
+        </div>
+        <div class="btn-bar">
+            <el-button type="primary" size="small">发送</el-button>
+        </div>
     </div>
     <div class="comment-list">
-
+        <div class="item-comment">
+            <div class="comment-info">
+                <el-avatar size="large" :src="avatarUrl"></el-avatar>
+                <div class="info">
+                    <div></div>
+                </div>
+            </div>
+            <div class="content">
+                测试回复测试回复测试回复测试回复测试回复测试回复测试回复测试回复测试回复
+            </div>
+        </div>
     </div>
 </div>
 </template>
@@ -21,13 +38,14 @@ export default {
     name: "comment",
     data: function () {
         return {
-            sortWay: "最热"
+            sortWay: "最热",
+            newComment: "",
         }
     },
     props: {
-        avatarUrl:{
-            type:String,
-            default:"https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png"
+        avatarUrl: {
+            type: String,
+            default: "https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png"
         }
     },
     created() {},
